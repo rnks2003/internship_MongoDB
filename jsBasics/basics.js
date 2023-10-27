3bfab49decabfe826fdc1dc812ad13e2f94a0361
RNKS <84720611+rnks2003@users.noreply.github.com>
2023-10-27 10:20:40 +0530

    Update basics.js

--- jsBasics/basics.js
+++ jsBasics/basics.js
@@ -1,9 +1,18 @@
-
+// Arrays 
 var arr = [0,"ele1",{key : "value"}]
 
+// objects
 var obj = {
     key1 : "value1",
     key2 : "value2"
 }
 
-console.log(arr,'\n',obj);
+// console logging
+console.log("\nConsole Logging : ");
+console.log("arr : ",arr,'\nobj : ',obj);
+
+// for loops
+console.log("\nFor loops : ");
+for( var i=0;i<arr.length;i++){
+    console.log(i," : ", arr[i]);
+}
