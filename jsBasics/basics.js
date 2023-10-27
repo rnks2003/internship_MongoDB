68f1bfd7ae4cbacdf73efc65621cb761752f8068
RNKS <84720611+rnks2003@users.noreply.github.com>
2023-10-27 10:28:27 +0530

    Update basics.js     .include arrays inside objects     .accessing object elements

--- jsBasics/basics.js
+++ jsBasics/basics.js
@@ -4,12 +4,13 @@ var arr = [0,"ele1",{key : "value"}]
 // objects
 var obj = {
     key1 : "value1",
-    key2 : "value2"
+    key2 : "value2",
+    key3 : arr
 }
 
 // console logging
 console.log("\nConsole Logging : ");
-console.log("arr : ",arr,'\nobj : ',obj);
+console.log("arr : ",arr,'\nobj.key1 : ',obj.key1);
 
 // for loops
 console.log("\nFor loops : ");
