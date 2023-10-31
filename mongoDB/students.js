5f83089b918ee8f7157cfc111fbf676e5d66dcde
RNKS <84720611+rnks2003@users.noreply.github.com>
2023-10-31 09:07:20 +0530

    update notes, Create dummy data

---
+++ mongoDB/students.js
@@ -0,0 +1,241 @@
+[{
+        name:"Ravi Narayana K S",
+        age:20,
+        phNo:9606374803,
+        gender:"Male",
+        hobbies:["Designing","Cooking"],
+        address:{
+            state:"Karnataka",
+            city:"Puttur",
+            pinCode:574201
+        }
+    },
+    {
+        name:"Subrahmanya Shivaram Hegde",
+        age:20,
+        phNo:7019742330,
+        gender:"Male",
+        hobbies:["Playing","Cooking"],
+        address:{
+            state:"Karnataka",
+            city:"Siddapur",
+            pinCode:581322
+        }
+    },
+    {
+        name:"Subrahmanya Prasad P S",
+        age:20,
+        phNo:7676435080,
+        gender:"Male",
+        hobbies:["Playing","Binge Watching"],
+        address:{
+            state:"Karnataka",
+            city:"Puttur",
+            pinCode:574201
+        }
+    },
+    {
+        name:"Shreyas Rao",
+        age:20,
+        phNo:8762387438,
+        gender:"Male",
+        hobbies:["Photography","Binge Watching"],
+        address:{
+            state:"Karnataka",
+            city:"Puttur",
+            pinCode:574201
+        }
+    },
+    {
+        name:"Anwesh Krishna B",
+        age:20,
+        phNo:9686543974,
+        gender:"Male",
+        hobbies:["Photography","Binge Watching"],
+        address:{
+            state:"Karnataka",
+            city:"Puttur",
+            pinCode:574201
+        }
+    },
+    {
+        name:"Ashwath Gowda",
+        age:20,
+        phNo:7337739939,
+        gender:"Male",
+        hobbies:["Craft","Acting"],
+        address:{
+            state:"Karnataka",
+            city:"Puttur",
+            pinCode:574201
+        }
+    },
+    {
+        name:"Ravikiran Sathyanarayana Hegde",
+        age:20,
+        phNo:9353147409,
+        gender:"Male",
+        hobbies:["Coding","Gaming"],
+        address:{
+            state:"Karnataka",
+            city:"Puttur",
+            pinCode:574201
+        }
+    },
+    {
+        name:"Hemanth Bhaskar Kamath",
+        age:20,
+        phNo:9482690447,
+        gender:"Male",
+        hobbies:["Binge Watching","Gaming"],
+        address:{
+            state:"Karnataka",
+            city:"Murudeshwara",
+            pinCode:574201
+        }
+    },
+    {
+        name:"Adithya Sharma",
+        age:20,
+        phNo:6362585433,
+        gender:"Male",
+        hobbies:["Binge Watching","Gaming"],
+        address:{
+            state:"Karnataka",
+            city:"Bengaluru",
+            pinCode:574201
+        }
+    },
+    {
+        name:"Shricharana",
+        age:20,
+        phNo:8431668249,
+        gender:"Male",
+        hobbies:["Trekking","Travelling"],
+        address:{
+            state:"Karnataka",
+            city:"Eeshwaramangala",
+            pinCode:574201
+        }
+    },
+    {
+        name:"Ram",
+        age:20,
+        phNo:9606374083,
+        gender:"Male",
+        hobbies:["Gaming","Cooking"],
+        address:{
+            state:"Karnataka",
+            city:"Mangaluru",
+            pinCode:574210
+        }
+    },
+    {
+        name:"Shivaram",
+        age:20,
+        phNo:7019472330,
+        gender:"Male",
+        hobbies:["Playing"],
+        address:{
+            state:"Karnataka",
+            city:"Sirsi",
+            pinCode:581332
+        }
+    },
+    {
+        name:"Prasad M",
+        age:20,
+        phNo:7676405380,
+        gender:"Male",
+        hobbies:["Binge Watching"],
+        address:{
+            state:"Karnataka",
+            city:"Udupi",
+            pinCode:548021
+        }
+    },
+    {
+        name:"Suman Rao",
+        age:20,
+        phNo:8762378238,
+        gender:"Male",
+        hobbies:["Gaming","Binge Watching"],
+        address:{
+            state:"Karnataka",
+            city:"Nehru Nagara",
+            pinCode:574201
+        }
+    },
+    {
+        name:"Krishna B",
+        age:20,
+        phNo:9686543234,
+        gender:"Male",
+        hobbies:["Photography","Binge Watching"],
+        address:{
+            state:"Karnataka",
+            city:"Puttur",
+            pinCode:574201
+        }
+    },
+    {
+        name:"Ashwatthama",
+        age:20,
+        phNo:7337739939,
+        gender:"Male",
+        hobbies:["Craft"],
+        address:{
+            state:"Karnataka",
+            city:"Manglore",
+            pinCode:574210
+        }
+    },
+    {
+        name:"Sathyanarayana Hegde",
+        age:20,
+        phNo:9353417409,
+        gender:"Male",
+        hobbies:["Gaming"],
+        address:{
+            state:"Karnataka",
+            city:"Puttur",
+            pinCode:574201
+        }
+    },
+    {
+        name:"Bhaskar",
+        age:20,
+        phNo:9482644907,
+        gender:"Male",
+        hobbies:["Binge Watching"],
+        address:{
+            state:"Karnataka",
+            city:"Manglore",
+            pinCode:574102
+        }
+    },
+    {
+        name:"Adithya",
+        age:20,
+        phNo:6362585433,
+        gender:"Male",
+        hobbies:["Reading","Gaming"],
+        address:{
+            state:"Karnataka",
+            city:"Bengaluru",
+            pinCode:574201
+        }
+    },
+    {
+        name:"Abhiram",
+        age:20,
+        phNo:8431667569,
+        gender:"Male",
+        hobbies:["Travelling"],
+        address:{
+            state:"Karnataka",
+            city:"Puttur",
+            pinCode:574201
+        }
+    }
+]
